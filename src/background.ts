3b4bd694c02e036a304d2c657f44509228820a79
anil.senocak <anil.senocak@turkcell.com.tr>
2025-03-28 10:23:07 +0300

    refactored

--- src/background.ts
+++ src/background.ts
@@ -44,8 +44,30 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
                       if (settings.styles.messages) {
                           const messagesCSS = `
                           /* CSS selectors for web4.bip.com */
-                          .message, .chat-message, .message-content, .message-text, .message-body {
+                          div[itemtype="messages"] div[class^="_textBubble__card_info__message_container_"], 
+                          div[itemtype="messages"] div[class^="_textBubble__card_info__message_content_text_"], 
+                          div[itemtype="messages"] div[class^="video-element"], 
+                          div[itemtype="messages"] div[class^="document-element"], 
+                          div[itemtype="messages"] div[class^="image-element"],
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__image"], 
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__message_container__text_"], 
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__info_"], 
+                          div[itemtype="messages"] div[class^="_replyContent_"], 
+                          div[itemtype="messages"] div[class^="_content_"], 
+                          div[itemtype="messages"] div[class^="_text_"] {
                               filter: blur(${settings.varStyles.msBlur}px) grayscale(1) !important;
+                          }
+                          div[itemtype="messages"] div[class^="_textBubble__card_info__message_container_"]:hover, 
+                          div[itemtype="messages"] div[class^="_textBubble__card_info__message_content_text_"]:hover, 
+                          div[itemtype="messages"] div[class^="video-element"]:hover, 
+                          div[itemtype="messages"] div[class^="document-element"]:hover, 
+                          div[itemtype="messages"] div[class^="image-element"]:hover, 
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__message_container__text_"]:hover, 
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__info_"]:hover, 
+                          div[itemtype="messages"] div[class^="_replyContent_"]:hover, 
+                          div[itemtype="messages"] div[class^="_content_"]:hover, 
+                          div[itemtype="messages"] div[class^="_text_"]:hover {
+                              filter: blur(0) grayscale(0) !important;
                           }
                           `;
                           chrome.scripting.executeScript({
@@ -57,10 +79,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
                       if (settings.styles.messagesPreview) {
                           const messagesPreviewCSS = `
-                          /* CSS selectors for web4.bip.com */
-                          .message-preview, .preview-text, .chat-preview, .preview-content {
-                            filter: blur(${settings.varStyles.mspBlur}px) grayscale(1);
-                            transition-delay: 0s;
+                          div[class^="_contact__content__body__message"] {
+                              filter: blur(${settings.varStyles.mspBlur}px) grayscale(1);
+                          }
+                          div[class^="_contact__content__body__message"]:hover {
+                              filter: blur(0) grayscale(0) !important;
                           }
                           `;
                           chrome.scripting.executeScript({
@@ -72,9 +95,19 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
                       if (settings.styles.mediaPreview) {
                           const mediaPreviewCSS = `
-                          /* CSS selectors for web4.bip.com */
-                          .media-preview, .image-preview, .video-preview, .attachment-preview, img, video {
+                          div[itemtype="messages"] div[class^="video-element"], 
+                          div[itemtype="messages"] div[class^="document-element"], 
+                          div[itemtype="messages"] div[class^="image-element"],
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__image"] {
                             filter: blur(${settings.varStyles.mdpBlur}px) grayscale(1);
+                            transition: initial;
+                            transition-delay: 0s;
+                          }
+                          div[itemtype="messages"] div[class^="video-element"]:hover,
+                          div[itemtype="messages"] div[class^="document-element"]:hover,
+                          div[itemtype="messages"] div[class^="image-element"]:hover,
+                          div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__image"]:hover {
+                            filter: blur(0) grayscale(0);
                             transition: initial;
                             transition-delay: 0s;
                           }
@@ -88,9 +121,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
                       if (settings.styles.textInput) {
                           const textInputDescriptionCSS = `
-                          /* CSS selectors for web4.bip.com */
-                          .text-input, .message-input, .chat-input, textarea, input[type="text"] {
+                          .text-input, .message-input, .chat-input, textarea, input[type="text"], editor-paragraph, .editor-paragraph, #editor-paragraph {
                             filter: blur(8px) opacity(0.25);
+                          }
+                          .text-input:hover, .message-input:hover, .chat-input:hover, textarea:hover, input[type="text"]:hover {
+                            filter: blur(0) opacity(1);
                           }
                           `;
                           chrome.scripting.executeScript({
@@ -102,9 +137,12 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
                       if (settings.styles.profilePic) {
                           const profilePicDescriptionCSS = `
-                          /* CSS selectors for web4.bip.com */
-                          .profile-pic, .avatar, .user-avatar, .profile-image, .user-pic, .user-image {
+                          ._avatar_1vdaa_1 {
                             filter: blur(${settings.varStyles.ppBlur}px) grayscale(1);
+                            transition-delay: 0s;
+                          }
+                          ._avatar_1vdaa_1:hover {
+                            filter: blur(0) grayscale(0);
                             transition-delay: 0s;
                           }
                           `;
@@ -117,10 +155,19 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
 
                       if (settings.styles.name) {
                           const nameDescriptionCSS = `
-                          /* CSS selectors for web4.bip.com */
-                          .user-name, .username, .display-name, .chat-name, .contact-name, .name {
+                          div[class^="_contact__content__header__name_"],
+                          span[class^="_message__nick_"],
+                          div[class^="_username_"],
+                          h3[class^="_info_card__title_"]
+                          {
                             filter: blur(${settings.varStyles.nmBlur}px) grayscale(1);
-                            transition-delay: 0s;
+                          }
+                          div[class^="_contact__content__header__name_"]:hover,
+                          span[class^="_message__nick_"]:hover,
+                          div[class^="_username_"]:hover,
+                          h3[class^="_info_card__title_"]:hover
+                          {
+                            filter: blur(0) grayscale(0);
                           }
                           `;
                           chrome.scripting.executeScript({
@@ -231,8 +278,30 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
                       // If no settings are found, apply default CSS
                       const defaultMessagesCSS = `
                       /* CSS selectors for web4.bip.com */
-                      .message, .chat-message, .message-content, .message-text, .message-body {
+                      div[itemtype="messages"] div[class^="_textBubble__card_info__message_container_"], 
+                      div[itemtype="messages"] div[class^="_textBubble__card_info__message_content_text_"], 
+                      div[itemtype="messages"] div[class^="video-element"], 
+                      div[itemtype="messages"] div[class^="document-element"], 
+                      div[itemtype="messages"] div[class^="image-element"],
+                      div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__image"], 
+                      div[itemtype="messages"] div[class^="_richLinkBubble__card_info__message_container__text_"], 
+                      div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__info_"], 
+                      div[itemtype="messages"] div[class^="_replyContent_"], 
+                      div[itemtype="messages"] div[class^="_content_"], 
+                      div[itemtype="messages"] div[class^="_text_"] {
                           filter: blur(8px) grayscale(1) !important;
+                      }
+                      div[itemtype="messages"] div[class^="_textBubble__card_info__message_container_"]:hover, 
+                      div[itemtype="messages"] div[class^="_textBubble__card_info__message_content_text_"]:hover, 
+                      div[itemtype="messages"] div[class^="video-element"]:hover, 
+                      div[itemtype="messages"] div[class^="document-element"]:hover, 
+                      div[itemtype="messages"] div[class^="image-element"]:hover, 
+                      div[itemtype="messages"] div[class^="_richLinkBubble__card_info__message_container__text_"]:hover, 
+                      div[itemtype="messages"] div[class^="_richLinkBubble__card_info__content__info_"]:hover, 
+                      div[itemtype="messages"] div[class^="_replyContent_"]:hover, 
+                      div[itemtype="messages"] div[class^="_content_"]:hover, 
+                      div[itemtype="messages"] div[class^="_text_"]:hover {
+                          filter: blur(0) grayscale(0) !important;
                       }
                       `;
                       chrome.scripting.executeScript({
